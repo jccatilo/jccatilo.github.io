925ed74f2dec701ee4ff25a4a74bdfcd17403e92
Godwin Dalawampu <46600161+jccatilo@users.noreply.github.com>
2026-01-08 16:38:06 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
@@ -10,5 +10,5 @@ export default defineConfig(({ mode }) => ({
   preview: {
     port: 4173
   },
-  base: mode === 'production' ? '/jc-portfolio-2026/' : '/', // Only use base path in production
-}))
+  base: '/',
+})
